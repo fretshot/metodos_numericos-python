96febc51817e6b455c234555823d3de717b4a4c6
Oscar Alemán <fretshot@gmail.com>
2019-05-19 17:37:18 -0500

    updated RK2Orden to get y2

--- rungeKutta2doOrden.py
+++ rungeKutta2doOrden.py
@@ -1,6 +1,7 @@
 import math
 
 def rungeKutta2doOrden():
+
     y0 = 3
     h = 0.2
     t0 = 0
@@ -12,6 +13,19 @@ def rungeKutta2doOrden():
     print("k1 = ",f'{k1:.8f}')
     print("k2 = ",f'{k2:.8f}')
     print("y1 = ",f'{y1:.8f}')
+
+    print("\n")
+
+    h = 0.2
+    t1 = t0 + h
+
+    k1 = h * funcion(y1,t1)
+    k2 = h * funcion((y1+k1),(t1+h))
+    y2 = y1 + ((k1+k2)/2)
+    
+    print("k1 = ",f'{k1:.8f}')
+    print("k2 = ",f'{k2:.8f}')
+    print("y2 = ",f'{y2:.8f}')
 
 def funcion(y,t):
     yx = (-y * math.cos(t)) / (1 - math.sin(t))
