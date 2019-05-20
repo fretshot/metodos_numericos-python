60822dfd142c49c9fa6d7de6e03ed006e3cdb0db
Oscar Alemán <fretshot@gmail.com>
2019-05-19 20:25:30 -0500

    added rungeKutta4toOrden38Simpson method

--- rungeKutta4toOrden38Simpson.py
+++ rungeKutta4toOrden38Simpson.py
@@ -1,18 +1,18 @@
 import math
 
-def rungeKutta4toOrden13Simpson():
+def rungeKutta4toOrden38Simpson():
 
-    print("\n\tRunge - Kutta 4to Orden 1/3 Simpson\n")
+    print("\n\tRunge - Kutta 4to Orden 3/8 Simpson\n")
 
     y0 = 3
     h = 0.2
     t0 = 0
 
     k1 = h * funcion(y0,t0)
-    k2 = h * funcion((y0+(k1/2)),(t0+(h/2)))
-    k3 = h * funcion((y0+(k2/2)),(t0+(h/2)))
-    k4 = h * funcion((y0+k3),(t0+h))
-    y1 = y0 + ((1/6)*(k1+(2*k2)+(2*k3)+k4))
+    k2 = h * funcion((y0+(k1/3)),(t0+(h/3)))
+    k3 = h * funcion((y0+(k1/3)+(k2/3)),(t0+((2/3)*h)))
+    k4 = h * funcion((y0+k1-k2+k3),(t0+h))
+    y1 = y0 + ((1/8)*(k1+(3*k2)+(3*k3)+k4))
     
     print("k1 = ",f'{k1:.9f}')
     print("k2 = ",f'{k2:.9f}')
@@ -26,10 +26,10 @@ def rungeKutta4toOrden13Simpson():
     t1 = t0 + h
 
     k1 = h * funcion(y1,t1)
-    k2 = h * funcion((y1+(k1/2)),(t1+(h/2)))
-    k3 = h * funcion((y1+(k2/2)),(t1+(h/2)))
-    k4 = h * funcion((y1+k3),(t1+h))
-    y2 = y1 + ((1/6)*(k1+(2*k2)+(2*k3)+k4))
+    k2 = h * funcion((y1+(k1/3)),(t1+(h/3)))
+    k3 = h * funcion((y1+(k1/3)+(k2/3)),(t1+((2/3)*h)))
+    k4 = h * funcion((y1+k1-k2+k3),(t1+h))
+    y2 = y1 + ((1/8)*(k1+(3*k2)+(3*k3)+k4))
     
     print("k1 = ",f'{k1:.9f}')
     print("k2 = ",f'{k2:.9f}')
@@ -41,4 +41,4 @@ def funcion(y,t):
     yx = (-y * math.cos(t)) / (1 - math.sin(t))
     return yx
 
-rungeKutta4toOrden13Simpson()
+rungeKutta4toOrden38Simpson()
